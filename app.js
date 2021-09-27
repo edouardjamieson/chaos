53adc1572611c8d8acecceef0e63e6afaa8643fc
edouardjamieson99@gmail.com <info@fatfish.ca>
2021-09-27 17:25:13 -0400

    Essaie d'un script qui gère la connexion

--- app.js
+++ app.js
@@ -14,6 +14,23 @@
  sockets(app)
  
  let dead_timeout
+
+ /***
+ *    ########  ##          ###    ##    ## ######## ########   ######  
+ *    ##     ## ##         ## ##    ##  ##  ##       ##     ## ##    ## 
+ *    ##     ## ##        ##   ##    ####   ##       ##     ## ##       
+ *    ########  ##       ##     ##    ##    ######   ########   ######  
+ *    ##        ##       #########    ##    ##       ##   ##         ## 
+ *    ##        ##       ##     ##    ##    ##       ##    ##  ##    ## 
+ *    ##        ######## ##     ##    ##    ######## ##     ##  ######  
+ */
+let players = {}
+function add_player(client) {
+    const player = `player${Object.keys(players).length}`
+    players[players] = client
+}
+
+
  
  /***
   *    ########   #######  ##     ## ######## ########  ######  
@@ -27,8 +44,20 @@
  
  //sockets
  app.ws('/socket', (ws, req) => {
+
+    ws.on('connection', (client) => {
+
+        if(Object.keys(players).length <= 4) {
+            add_player(client)
+            ws.send('__join__')
+        }else{
+            //Si trop de joueur on kickout le joueur qui essaie de se connecter
+            ws.send('__kill__:too_many_players')
+        }
+
+    })
  
-     ws.on('message', (msg) => {
+    ws.on('message', (msg) => {
          
          if(msg === '__pong__') {
              clearTimeout(dead_timeout)
